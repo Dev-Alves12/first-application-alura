7203043d1551fcf697016aef5f841402f27218fc
José Henrique <213022@upf.br>
2026-03-25 10:19:06 -0300

    primeira-aplicação

---
+++ exercicios.py
@@ -0,0 +1,40 @@
+# numero = int(input("Digite um número: "))
+# if numero % 2 == 0:
+#     print("O número é par.")
+# else:
+#     print("O número é ímpar.")
+
+    
+
+# idade = int(input("Digite sua idade: "))
+# if 0 < idade <= 12:
+#     print("Criança")
+# elif 12 < idade < 18:
+#     print("Adolescente")
+# elif idade >= 18:
+#     print("Adulto")
+# else: 
+#     print("Valor inválido!")
+
+
+
+# usuario_correto = "alura"
+# senha_correta = "alura123"
+
+# usuario = input("Digite o nome de usuário: ")
+# senha = input("Digite a senha: ")
+
+# if usuario == usuario_correto and senha == senha_correta:
+#     print("Login bem sucedido!")
+# else:
+#     print("Credenciais inválidas. Tente novamente.")
+
+
+# lista_de_numeros = [1,2,3,4,5,6,7,8,9]
+# lista_de_nomes = ['jose','maeia','gui','lais']
+# lista_de_anos = [1999,2026]
+# for numero in lista_de_numeros:
+#     print(numero)
+
+# info_pessoa = {'nome': 'José', 'idade': 18, 'cidade': 'Vila Lângaro'}
+# pessoa = {'nome': 'Felipe', 'idade': 30, 'cidade': 'São Paulo'}
